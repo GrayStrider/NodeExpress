bff46321b3a2bf99bb01e62fc7c61fabdbb2a6a5
GrayStrider <GrayStrider@users.noreply.github.com>
2018-12-10 18:13:34 +1100

    [10 Dec 18, Mon]

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const dev_title = 'Debug - ';
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', {title: 'Express'});
+	res.render('index', {title: dev_title + 'Express!'});
 });
 
 // Test route //
 router.get('/test', function (req, res, next) {
-	res.render('test', {title: 'test'});
+	res.render('test', {title: dev_title + 'Test'});
 });
 
 module.exports = router;
